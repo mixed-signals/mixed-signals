ead7cc5f997df7c5cf4773ab50e09da75e4da004
junjungoal <junyamada107@gmail.com>
2018-05-16 21:38:44 +0900

    add simple NN

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,8 +3,7 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Lib
-    ( someFunc
-    ) where
+    ( someFunc) where
 import qualified Prelude
 import Data.Array.Accelerate-- (Exp(..),Acc, Array(..),DIM1,DIM2,Num,Floating,Scalar,Z,(:.))
 import Data.Array.Accelerate.Interpreter(run)
@@ -12,6 +11,9 @@ import Debug.Trace
 
 sigmoid :: (Prelude.Floating e) => e -> e
 sigmoid x = 1 / (1 + exp(-x))
+
+dsigmoid :: (Prelude.Num e) => e -> e
+dsigmoid y = y * (1 - y)
 
 
 (<.>) :: forall e. Num e => Acc (Array DIM2 e) -> Acc (Array DIM2 e) -> Acc (Array DIM2 e)
@@ -57,16 +59,24 @@ rowVector x = reshape sh x
         (Z :. n) = unlift (shape x) :: Z :. Exp Int
         sh = lift (Z :. (1 :: Exp Int) :. n) :: Exp DIM2
 
-square :: Num e => e -> e
+square :: Prelude.Num e => e -> e
 square x = x * x
 
-network :: (Num e, Elt e, Floating e) => Acc (Array DIM1 e) -> Acc (Array DIM2 e) -> Acc (Array DIM2 e) -> Exp e
-network x w1 w2 t
-    = err
+
+train :: (Num e, Elt e, Floating e, FromIntegral Int e) => Exp e -> Acc (Array DIM1 e) -> Acc (Array DIM2 e) -> Acc (Array DIM2 e) -> Acc (Array DIM1 e) -> (Exp e, Acc (Array DIM2 e), Acc (Array DIM2 e))
+train η x w1 w2 t
+    = trace (Prelude.show $ run dy)
+    $ trace (Prelude.show $ run dx4)
+    $ trace (Prelude.show $ run dw2)
+    $ trace (Prelude.show $ run dx3)
+    $ trace (Prelude.show $ run dx2)
+    $ trace (Prelude.show $ run dw1)
+    $ trace (Prelude.show $ run dx1) (err, w1', w2')
     where
         -- w1 :: n x m
         -- x :: n
-        x2 = rowVector x <.> w1
+        x1 = rowVector x
+        x2 = x1 <.> w1
         -- x2 :: 1 x m
         x3 = map sigmoid x2
         -- x3 :: 1 x m
@@ -74,19 +84,33 @@ network x w1 w2 t
         x4 = x3 <.> w2
         -- x4 :: 1 x p
         x5 = map sigmoid x4
+
         y = reshape (shape t) x5
-        err = 1/(2 * fromIntegral (size y)) * the (fold (+) 0 (map square (zipWith (-) y t)))
-        -- dy = 
+        z = map (\v -> v / fromIntegral (size y)) $ zipWith (-) y t
+        err = 1/2 * the (fold (+) 0 $ map square z)
+        dy = rowVector z
+        dx4 = zipWith (*) (map dsigmoid x5) dy
+        dw2 = transpose x3 <.> dx4
+        dx3 = dx4 <.> transpose w2
+        dx2 = zipWith (*) (map dsigmoid x3) dx3
+        dw1 = transpose x1 <.> dx2
+        dx1 = dx2 <.> transpose w1
+        update = zipWith (\w dw -> w - η*dw)
+        w1' = update w1 dw1
+        w2' = update w2 dw2
 
 testNetwork :: Acc (Scalar Double)
 testNetwork
-    = unit err
+    = trace (Prelude.show $ run w1' )
+    $ trace (Prelude.show $ run w2' )
+    $ unit err
     where
         x = use (fromList (Z :. 1) [3]) :: Acc (Array DIM1 Double)
         w1 = use (fromList (Z :. 1 :. 2) [0.3, 0.3])
         w2 = use (fromList (Z :. 2 :. 1) [0.3, 0.3])
         t = map classify x -- t :: Z :. 1
-        err = network x w1 w2 t
+        lr = 1.0e-2
+        (err, w1', w2') = train lr x w1 w2 t
 
 
 someFunc :: Prelude.IO ()
