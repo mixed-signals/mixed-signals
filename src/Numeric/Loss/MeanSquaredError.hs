13d827a25b0dae647c93ec4e2186f40e86c56d48
Viktor Kronvall <viktor.kronvall@gmail.com>
2018-06-25 23:23:46 +0900

    Make specific `Loss` class

--- src/Numeric/Loss/MeanSquaredError.hs
+++ src/Numeric/Loss/MeanSquaredError.hs
@@ -2,30 +2,26 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Numeric.Loss.MeanSquaredError where
 
-import Numeric.Layer
+import Numeric.Loss
 import Dependent.Size
 import qualified Numeric.Vector.Sized as Sized
 import Numeric.Vector.Sized(SizedVector',SizedVector)
 import Data.Proxy
 import GHC.TypeLits(KnownNat,Nat,natVal)
 
-newtype MeanSquaredError (n :: Nat) = MeanSquaredError { mseTargets :: SizedVector' n}
+newtype MeanSquaredError (n :: Nat) = MeanSquaredError ()
 
-instance forall n. KnownNat n => Layer (MeanSquaredError n) where
-  type Inputs (MeanSquaredError n) = ZZ ::. n
-  type Outputs (MeanSquaredError n) = ZZ
-  type Tape (MeanSquaredError n) = SizedVector n
-  type Gradient (MeanSquaredError n) = ()
-  forward (MeanSquaredError targets) x
-    = (err, x)
-      where
-        err = (normalize . Sized.foldAll (+) 0 . Sized.map square) (Sized.zipWith (-) x (Sized.use targets))
-        square x = x ^2
-        n = fromInteger $ natVal (Proxy :: Proxy (n :: Nat))
-        normalize total = Sized.map (\t -> t / n) total
+instance forall n. KnownNat n => Loss (ZZ ::. n) (MeanSquaredError n) where
+  -- forward (MeanSquaredError targets) x
+  --   = (err, x)
+  --     where
+  --       err = (normalize . Sized.foldAll (+) 0 . Sized.map square) (Sized.zipWith (-) x (Sized.use targets))
+  --       square x = x ^2
+  --       n = fromInteger $ natVal (Proxy :: Proxy (n :: Nat))
+  --       normalize total = Sized.map (\t -> t / n) total
 
-  backward (MeanSquaredError targets) x _ = ((), dx)
+  lossDerivative _ x targets = dx
     where
-      dx = normalize (Sized.zipWith (-) x (Sized.use targets))
+      dx = normalize (Sized.zipWith (-) x targets)
       n = fromInteger $ natVal (Proxy :: Proxy (n :: Nat))
       normalize zs = Sized.map (\z -> 2/n * z) zs
